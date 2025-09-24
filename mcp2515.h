d41556510ea019948e519ac231c5a7e1c6b5f2f9
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-09-24 16:28:55 +0200

    initial

---
+++ mcp2515.h
@@ -0,0 +1,12 @@
+#ifndef MCP2515_MCP2515_H
+#define MCP2515_MCP2515_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MCP2515_MCP2515_H
---
+++ mcp2515_utility.h
@@ -0,0 +1,12 @@
+#ifndef MCP2515_MCP2515_UTILITY_H
+#define MCP2515_MCP2515_UTILITY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MCP2515_MCP2515_UTILITY_H
